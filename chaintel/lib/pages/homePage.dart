e360e2085d88abeb6fabbbaca5ba468d9394dc19
xena <1runx3na2@gmail.com>
2021-08-21 00:54:42 +0530

    Home Appbar complete

--- chaintel/lib/pages/homePage.dart
+++ chaintel/lib/pages/homePage.dart
@@ -1,7 +1,6 @@
 import 'package:chaintel/pages/homeUser.dart';
 import 'package:flutter/material.dart';
 import 'package:chaintel/pages/profilePage.dart';
-import 'package:chaintel/pages/homeUser.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -44,7 +43,6 @@ class _MyHomePage extends State<MyHomePage> {
           onTap: (index) {
             setState(() {
               currentPage = index;
-              print(currentPage);
             });
           },
           letIndexChange: (index) => true,
@@ -62,9 +60,11 @@ class _MyHomePage extends State<MyHomePage> {
   
   _getPage(int page) {
     switch (page) {
-      case 0:
-        return ProfilePage();
+      // case 0:
+      //   return ProfilePage();
       case 1:
+        return ProfilePage();
+      case 2:
         return HomePage();
       default:
         return Column(
--- chaintel/lib/pages/homeUser.dart
+++ chaintel/lib/pages/homeUser.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override 
+  _HomePage createState() => _HomePage();
+}
+
+appBarHome() {
+  return  PreferredSize(
+    preferredSize: Size.fromHeight(5.0),
+    child: AppBar(
+    backgroundColor: Color(0xFF2BD3C0),
+  ));
+}
+
+ 
+class _HomePage extends State<HomePage> {
   @override 
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Home Page"),);
+    return Scaffold(
+      appBar: appBarHome(),
+      body: Container(
+        //decoration: BoxDecoration(gradient: LinearGradient(colors: [Color(0xFF2BD3C0),Color(0xFF983590)],begin: Alignment.bottomCenter,end: Alignment.topCenter), ),
+      ),
+    );
   }
 }
