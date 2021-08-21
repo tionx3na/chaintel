94c7ea21e6427546e453395bc6ffd3a0cf47cd8e
xena <1runx3na2@gmail.com>
2021-08-21 16:13:23 +0530

    Home Page Cards UI

--- chaintel/lib/pages/homeUser.dart
+++ chaintel/lib/pages/homeUser.dart
@@ -24,11 +24,10 @@ class _HomePage extends State<HomePage> {
       appBar: appBarHome(),
       body: Container(
         //decoration: BoxDecoration(gradient: LinearGradient(colors: [Color(0xFF2BD3C0),Color(0xFF983590)],begin: Alignment.bottomCenter,end: Alignment.topCenter), ),
-        child: SingleChildScrollView(
         child: Column(
           children: [
             Container(
-              height: MediaQuery.of(context).size.height/4,
+              height: MediaQuery.of(context).size.height/1.16,
               width: MediaQuery.of(context).size.width,
               child: Stack(
                 children: [
@@ -36,63 +35,134 @@ class _HomePage extends State<HomePage> {
                     top: 0,//MediaQuery.of(context).size.height,
                     left: 0,//MediaQuery.of(context).size.width,
                     right: 0,
-                    bottom: 70,
+                    bottom: 550,
                     child: Container(
-                        color: Color(0xFF2BD3C0),
+                        decoration: BoxDecoration(
+                          color: Color(0xFF2BD3C0),
+                          image: DecorationImage(
+                              image: AssetImage("assets/images/app_bar.jpg"),
+                              fit: BoxFit.cover,
+                              colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.1), BlendMode.dstATop),
+                              )
+                        ),
                     ),
                   ),
+
                   Positioned(
-                    top: 20,
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    child: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      child:  ClipRRect(
-                        child: Image.asset('assets/images/logo.png'),
-                        borderRadius: BorderRadius.circular(50.0),
-                    ),
-                    ),
-                    )
-                ],
-              ),
-              //Center(child:Image.asset('assets/images/logo.png', width: 250,height: 250,))
+                        top: 120,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                      child: Container(
+                        //color: Colors.black,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Card(
+                                semanticContainer: true,
+                                clipBehavior: Clip.antiAliasWithSaveLayer,
+                                margin: EdgeInsets.all(30.0),
+                                color: Color(0xFF00BED7),
+                                child: Container(
+                                  height: 170,
+                                  width: 500,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage("assets/images/first_card.jpg"),
+                                      fit: BoxFit.cover,
+                                      colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop),
+                                    )
+                                  ),
+                                  //child: Image.asset('assets/images/first_card.jpg'),
+                                  
+                                ),
+                              ),
+                              Card(
+                                margin: EdgeInsets.all(20.0),
+                                color: Color(0xFF00A5E5),
+                                child: Container(
+                                  height: 170,
+                                  width: 500,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage("assets/images/first_card.jpg"),
+                                      fit: BoxFit.cover,
+                                      colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.dstATop),
+                                    )
+                                  ),
+                                ),
+                              ),
+                              Card(
+                                margin: EdgeInsets.all(20.0),
+                                color: Color(0xFF0087E0),
+                                child: Container(
+                                  height: 170,
+                                  width: 500,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage("assets/images/first_card.jpg"),
+                                      fit: BoxFit.cover,
+                                      colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop),
+                                    )
+                                  ),
+                                ),
+                              ),
+                               Card(
+                                margin: EdgeInsets.all(20.0),
+                                color: Color(0xFF7063C3),
+                                child: Container(
+                                  height: 170,
+                                  width: 500,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage("assets/images/first_card.jpg"),
+                                      fit: BoxFit.cover,
+                                      colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.dstATop),
+                                    )
+                                  ),
+                                ),
+                              ),
+                                Card(
+                                margin: EdgeInsets.all(20.0),
+                                color: Color(0xFF983590),
+                                child: Container(
+                                  height: 170,
+                                  width: 500,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage("assets/images/first_card.jpg"),
+                                      fit: BoxFit.cover,
+                                      colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.dstATop),
+                                    )
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      ),
+
+                    Positioned(
+                      top: 0,
+                      left: 100,
+                      right: 100,
+                      bottom: 450,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        child:  ClipRRect(
+                          child: Image.asset('assets/images/logo.png'),
+                          borderRadius: BorderRadius.circular(50.0),
+                      ),
+                      ),
+                      ),
+                  ],
+                ),
+                      //Center(child:Image.asset('assets/images/logo.png', width: 250,height: 250,))
             ),
-            Container(
-              height: MediaQuery.of(context).size.height/1.7,
-              width: MediaQuery.of(context).size.width,
-              //color: Colors.black,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const SizedBox(width: 20.0, height: 100.0),
-                  const Text(
-                    'Be',
-                    style: TextStyle(fontSize: 43.0),
-                  ),
-                  const SizedBox(width: 20.0, height: 100.0),
-                  DefaultTextStyle(
-                    style: const TextStyle(
-                      fontSize: 40.0,
-                      color: Colors.red,
-                    ),
-                    child: AnimatedTextKit(
-                      animatedTexts: [
-                        RotateAnimatedText('AWESOME'),
-                        RotateAnimatedText('OPTIMISTIC'),
-                        RotateAnimatedText('DIFFERENT'),
-                      ],
-                      onTap: () {
-                        print("Tap Event");
-                      },
-                    ),
-                  ),
-                ],
-              )
-            ),
+            
           ],
         )),
-      ),
     );
   }
 }
